78984447daad0744ea4577b784a37a7389bcd3fd
Andreas A. Flakstad <andflak@gmail.com>
2014-03-05 11:46:25 +0100

    cleaned up view of site

--- models/menu.py
+++ models/menu.py
@@ -5,15 +5,16 @@
 ## Customize your APP title, subtitle and menus here
 #########################################################################
 
-response.logo = A(B('web',SPAN(2),'py'),XML('&trade;&nbsp;'),
-                  _class="brand",_href="http://www.web2py.com/")
-response.title = 'My House'
-response.subtitle = ''
+#response.logo = A(B('web',SPAN(2),'py'),XML('&trade;&nbsp;'),
+#                  _class="brand",_href="http://www.web2py.com/")
+response.logo = ''
+response.title = 'Casa del Hansen'
+response.subtitle = 'Hansenveien 29'
 
 ## read more at http://dev.w3.org/html5/markup/meta.name.html
-response.meta.author = 'Your Name <you@example.com>'
-response.meta.description = 'a cool new app'
-response.meta.keywords = 'web2py, python, framework'
+response.meta.author = 'Team Q>'
+response.meta.description = 'Visual Sensor Data for your Home'
+response.meta.keywords = 'web2py, python, home, electronics'
 response.meta.generator = 'Web2py Web Framework'
 
 ## your http://google.com/analytics id
@@ -27,7 +28,7 @@ response.menu = [
     (T('Home'), False, URL('default', 'index'), [])
 ]
 
-DEVELOPMENT_MENU = True
+DEVELOPMENT_MENU = False
 
 #########################################################################
 ## provide shortcuts for development. remove in production
