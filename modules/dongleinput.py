5114fc1daa0402f49b86fdafddcf083b29a96c8d
Andreas A. Flakstad <andflak@gmail.com>
2014-03-26 11:26:01 +0100

    module for receiving input data

---
+++ modules/dongleinput.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf8
+# dependency: pyserial, "pip install pyserial".
+import serial
+import time
+
+def setup():
+    ser = serial.Serial()
+    ser.baudrate = 38400
+    # setup input port. "python -m serial.tools.list_ports" to find the usb-port of the connected dongle.
+    ser.port = "/dev/cu.usbmodem1a121"
+    ser.stopbits = serial.STOPBITS_ONE
+    ser.rtscts = True
+    ser.open()
+    return ser.isOpen()
+
+def listen():
+    while(True):
+        # Reads one line, splits at a new line.
+        x = ser.readline()
+        # Data kommer i format: "UNIT ID sensortype data", for eksempel: "UNIT 1 0 20". La oss forsøke å lese hele linja og splitte på " ".
+        data = x.split(" ")
+        # Hvis det kommer noe annet enn UNIT er det snakk om config-meldinger i starten av oppsettet.
+        if (data[0] == "UNIT"):
+            sensor_id = data[1]
+            sensor_type = data[2]
+            sensor_value = data[3]
+        time.sleep(3000)
